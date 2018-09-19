8aa2d5ff79071ba951bfc65a9fdd6135a4a62646
Jack Baumann <wavejbaumann@gmail.com>
2018-09-19 13:52:00 -0500

    Cleanup

--- Test.py
+++ Test.py
@@ -7,6 +7,6 @@ Garph = Graph({(1, 2): 5, (2, 3): 3, (3, 9): 2, (1, 4): 1, (4, 6): 4, (4, 5): 6,
 print(Garph.connections)
 print(Garph.vertices)
 print(Garph.edges)
-print(dfs(Garph, 1, 9))
+print("Depth First Search: ", dfs(Garph, 1, 9))
 Garph.seen = set()
-print(dijkstra(Garph, 1, 9))
+print("Dijkstra:           ", dijkstra(Garph, 1, 9))
