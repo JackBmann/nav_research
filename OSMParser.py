eb36e71a687939bb68e5757b524a82e3cf288bc6
Jack Baumann <wavejbaumann@gmail.com>
2018-12-07 12:39:02 -0600

    Added more filtering, flipped two way edges

--- OSMParser.py
+++ OSMParser.py
@@ -5,11 +5,11 @@ from math import radians, sin, cos, asin, sqrt
 
 def haversine(vertex1, vertex2):
     """
-      Calculates the distance in kilometers between the GPS coordinates of the given vertices using the Haversine formula.
-      Our code is based off of information and code found here: https://rosettacode.org/wiki/Haversine_formula#Python
-      :param vertex1: the first vertex
-      :param vertex2: the second vertex
-      :return: the distance in kilometers between the two given vertexes
+    Calculates the distance in kilometers between the GPS coordinates of the given vertices using the Haversine formula.
+    Our code is based off of information and code found here: https://rosettacode.org/wiki/Haversine_formula#Python
+    :param vertex1: the first vertex
+    :param vertex2: the second vertex
+    :return: the distance in kilometers between the two given vertexes
     """
     earth_radius = 6372.8  # Earth radius in kilometers
     lat1 = radians(vertex1.get_latitude())
@@ -24,17 +24,29 @@ def haversine(vertex1, vertex2):
 
 def valid_feature(tags):
     """
-      Determines if the feature represented by the given tags is valid, i.e. a road
-      :param tags: a list of tags from an OSM feature
-      :return: true if the feature is valid, false otherwise
+    Determines if the feature represented by the given tags is valid, i.e. a road
+    :param tags: a list of tags from an OSM feature
+    :return: true if the feature is valid, false otherwise
     """
     if len(tags) == 0:
         return True
-    invalid_roads = ["footpath", "pedestrian", "track", "bus_guideway", "raceway", "bridleway", "steps", "path",\
-                     "proposed"]
+    invalid_roads = ["footpath", "pedestrian", "track", "bus_guideway", "raceway", "bridleway", "steps", "path",
+                     "proposed", "footway"]
     for tag in tags:
         if (tag.attrib["k"] == "highway" and tag.attrib["v"] not in invalid_roads) or \
            (tag.attrib["k"] == "route" and tag.attrib["v"] != "road"):
+            return True
+    return False
+
+
+def is_oneway(tags):
+    """
+    Determines if the way represented by the given tags is one-way
+    :param tags: a list of tags from an OSM way
+    :return: true if the way is one-way, false otherwise
+    """
+    for tag in tags:
+        if tag.attrib["k"] == "oneway" and tag.attrib["v"] == "yes":
             return True
     return False
 
@@ -70,6 +82,8 @@ def parse_osm(path):
             nds = child.findall('nd')
             # A list of all of the metadata of this way that is used to filter out unwanted ways and store metadata
             tags = child.findall('tag')
+            # Boolean flag to handle two-way edge flipping.
+            oneway = is_oneway(tags)
 
             if not valid_feature(tags):
                 continue
@@ -78,17 +92,24 @@ def parse_osm(path):
             first_vertex = None
             for node in nds:
                 ''' 
-                  There can be nodes that were filtered out already still in these ways, so if they are not in the
-                  vertices dictionary, we'll need to skip them. 
-                  For example if a road and a railway intersect, the point at which they intersect could still be
-                  in a way representing a road, even though it's a railway node.
+                There can be nodes that were filtered out already still in these ways, so if they are not in the
+                vertices dictionary, we'll need to skip them. 
+                For example if a road and a railway intersect, the point at which they intersect could still be
+                in a way representing a road, even though it's a railway node.
                 '''
                 if node.attrib['ref'] not in vertices:
                     continue
 
                 vertex = vertices[node.attrib['ref']]
                 if prev_vertex:
-                    edges.append(Edge(prev_vertex, vertex, abs(haversine(prev_vertex, vertex))))
+                    # The haversine distance between the two vertexes
+                    distance = abs(haversine(prev_vertex, vertex))
+
+                    edges.append(Edge(prev_vertex, vertex, distance))
+
+                    # If the way is one-way, flip the edge and append it
+                    if not oneway:
+                        edges.append(Edge(vertex, prev_vertex, distance))
                 else:
                     first_vertex = vertex
                 prev_vertex = vertex
@@ -112,10 +133,10 @@ def parse_osm(path):
                 mem_type = member.attrib['type']
                 mem_id = member.attrib['ref']
                 ''' 
-                  There can be nodes that were filtered out already still in these ways, so if they are not in the
-                  vertices dictionary, we'll need to skip them. 
-                  For example if a road and a railway intersect, the point at which they intersect could still be
-                  in a way representing a road, even though it's a railway node.
+                There can be nodes that were filtered out already still in these ways, so if they are not in the
+                vertices dictionary, we'll need to skip them. 
+                For example if a road and a railway intersect, the point at which they intersect could still be
+                in a way representing a road, even though it's a railway node.
                 '''
                 if mem_id not in vertices and mem_id not in ways:
                     continue
