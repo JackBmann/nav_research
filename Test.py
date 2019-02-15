cb4aa5954fbb5bbf333a4d4d1bbf4e40ad0cd4f7
Jack Baumann <wavejbaumann@gmail.com>
2019-02-15 13:15:47 -0600

    Restructured Test

--- Test.py
+++ Test.py
@@ -30,21 +30,22 @@ def get_test_graph():
 
 def test_save_graph_to_file():
     graph = get_test_graph()
+    print_graph(graph)
+    graph.write_graph("Test.txt")
+    new_graph = Graph.read_graph("test.txt")
+    print_graph(new_graph)
+    new_graph.write_graph("test.txt")
+
+
+def print_graph(graph):
     print("Graph Connections: ", graph.connections)
     print("Graph Vertices:    ", graph.vertices)
     print("Graph Edges:       ", graph.edges)
-    print()
-    graph.write_graph("Test.txt")
-    new_graph = Graph.read_graph("test.txt")
-    print("Connections:       ", new_graph.connections)
-    print("Vertices:          ", new_graph.vertices)
-    print("Edges:             ", new_graph.edges)
-    print()
-    new_graph.write_graph("test.txt")
 
 
 def test_graph_algorithm(graph, algorithm, src, dest, heuristic, name, filename):
     graph.clear_colors()
+    print_graph(graph)
     start = datetime.now()
     if heuristic:
         path = algorithm(graph, src, dest, heuristic)
@@ -63,47 +64,41 @@ def test_graph_algorithm(graph, algorithm, src, dest, heuristic, name, filename)
     draw_graph(networkx_graph, name, filename)
 
 
-def test_dfs():
-    graph = get_test_graph()
+def test_dfs(graph):
     test_graph_algorithm(graph, dfs, graph.get_vertex(0), graph.get_vertex(8), None,
                          "Depth First Search", "dfs")
 
 
-def test_dijkstra():
-    graph = get_test_graph()
+def test_dijkstra(graph):
     test_graph_algorithm(graph, dijkstra, graph.get_vertex(0), graph.get_vertex(8), djikstra_heuristic,
                          "Dijkstra", "dijkstra")
 
 
-def test_a_star():
-    graph = get_test_graph()
+def test_a_star(graph):
     test_graph_algorithm(graph, dijkstra, graph.get_vertex(0), graph.get_vertex(8), a_star_heuristic,
                          "A*", "a_star")
 
 
-def test_parse_osm():
-    # graph = read_shp(r"shapefiles/OSMCampus.shp", False)
-    graph = parse_osm('shapefiles/OSMCampus.osm')
+def test_parse_osm(osm_path):
+    # graph = read_shp(osm_path, False)
+    graph = parse_osm(osm_path)
     networkx_graph = graph.convert_networkx()
     draw_graph(networkx_graph, "OSM Campus", "osm_campus")
 
 
-def test_osm_dfs():
-    graph = parse_osm('shapefiles/OSMCampus.osm')
+def test_osm_dfs(graph):
     test_graph_algorithm(graph, dfs, graph.get_vertex(list(graph.vertices.keys())[0]),
                          graph.get_vertex(list(graph.vertices.keys())[47]), None,
                          "OSM DFS", "osm_dfs")
 
 
-def test_osm_dijkstra():
-    graph = parse_osm('shapefiles/OSMCampus.osm')
+def test_osm_dijkstra(graph):
     test_graph_algorithm(graph, dijkstra, graph.get_vertex(list(graph.vertices.keys())[0]),
                          graph.get_vertex(list(graph.vertices.keys())[47]), djikstra_heuristic,
                          "OSM Dijkstra", "osm_dijkstra")
 
 
-def test_osm_a_star():
-    graph = parse_osm('shapefiles/OSMCampus.osm')
+def test_osm_a_star(graph):
     test_graph_algorithm(graph, dijkstra, graph.get_vertex(list(graph.vertices.keys())[0]),
                          graph.get_vertex(list(graph.vertices.keys())[47]), a_star_heuristic,
                          "OSM A*", "osm_a_star")
@@ -111,10 +106,7 @@ def test_osm_a_star():
 
 def test_box_roads():
     graph = Graph.read_graph("BoxRoads.txt")
-    print("Connections:       ", graph.connections)
-    print("Vertices:          ", graph.vertices)
-    print("Edges:             ", graph.edges)
-    print()
+    print_graph(graph)
     test_graph_algorithm(graph, dijkstra, graph.get_vertex(0), graph.get_vertex(33), a_star_heuristic,
                          "Box Roads A*", "box_roads")
 
@@ -149,11 +141,13 @@ def fix_box_roads():
                     writeFile.write(p_line)
 
 
-# test_dfs()
-# test_dijkstra()
-# test_a_star()
-# test_parse_osm()
-test_osm_dfs()
-test_osm_dijkstra()
-test_osm_a_star()
-test_box_roads()
+# test_graph = get_test_graph()
+# test_dfs(test_graph)
+# test_dijkstra(test_graph)
+# test_a_star(test_graph)
+# test_parse_osm('shapefiles/OSMCampus.osm')
+osm_graph = parse_osm('shapefiles/OSMCampus.osm')
+test_osm_dfs(osm_graph)
+test_osm_dijkstra(osm_graph)
+# test_osm_a_star(osm_graph)
+# test_box_roads()
